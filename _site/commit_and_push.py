f913ef31022cf7cf48c833b4744732c0993602a7
Sam Wolfson <terabyte128@gmail.com>
2015-04-13 16:17:26 -0700

    added comments

--- _site/commit_and_push.py
+++ _site/commit_and_push.py
@@ -7,15 +7,16 @@ args = sys.argv
 
 if len(args) != 2:
     print("Usage: ./commit_and_push.py [commit message]")
+    exit()
 
-else:
-    print("adding all files...")
-    subprocess.call(['git', 'add', '.'])
 
-    print("committing...")
-    subprocess.call(['git', 'commit', '-m', args[1]])
+print("adding all files...")
+subprocess.call(['git', 'add', '.'])
 
-    print("pushing...")
-    subprocess.call(['git', 'push'])
-    
-    print("done!")
+print("committing...")
+subprocess.call(['git', 'commit', '-m', args[1]])
+
+print("pushing...")
+subprocess.call(['git', 'push'])
+
+print("done!")
